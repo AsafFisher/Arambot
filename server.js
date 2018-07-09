bd49458ce59239cac57cef4d9e243595f0d1d736
Asaf Fisher <asaffisher@icloud.com>
2018-07-09 14:33:48 -0400

    fixed

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const db = require('./discordbot.js');
 db.init(function(){
   dclient = db.client;
   dclient.on('ready', () => {
-<<<<<<< HEAD
         console.log(`Logged in as ${dclient.user.tag}!`);
-=======
-        console.log(`Logged in as ${client.user.tag}!`);
->>>>>>> 9bf8ed11724b5081618d6fbf755db1f1b186e285
   });
 
   dclient.on('message', msg => {
