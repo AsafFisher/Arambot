998a33dfe5028e12aa1ed413cbe372585c791b89
Asaf Fisher <asaffisher@icloud.com>
2018-07-09 14:22:41 -0400

    Changed discord init to discordbot.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Edited. Edited
-const Discord = require('discord.js');
-const fs = require('fs');
-const client = new Discord.Client();
+
+const client = require('./discordbot.js').client
 
 client.on('ready', () => {
       console.log(`Logged in as ${client.user.tag}!`);
@@ -11,10 +10,4 @@ client.on('message', msg => {
       if (msg.content === 'ping') {
 	      //msg.reply('pong');
 	}
-});
-//CTF!!!!! (find the flag and let me know so I will patch it (: )
-fs.readFile('secret.txt', (err, data) => {
-      if (err) throw err;
-        console.log(data.toString().split('\n')[0]);
-	client.login(data.toString().split('\n')[0]);
 });
