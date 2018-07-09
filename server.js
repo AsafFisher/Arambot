b00c1fa3643e2d6b31079011dcc104c8bd03adc5
Asaf Fisher <asaffisher@icloud.com>
2018-07-09 06:51:37 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-      console.log('Logged in as ${client.user.tag}!');
+      console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
