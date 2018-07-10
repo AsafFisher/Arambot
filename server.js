258caddfabf30642b6bc8855f05fc39c3aed79be
Asaf Fisher <asaffisher@icloud.com>
2018-07-10 02:21:40 -0400

    Added comments.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//Here is the file where you add stuff.
-var dclient;
+//Here is the file where you add stuff
+var dclient; // dclient is the bot client. Here you can controll the bot, push it to git and the bot manager will automatically deploy the bot.
 const db = require('./discordbot.js');
 db.init(function(){
   dclient = db.client;
   dclient.on('ready', () => {
         console.log(`Logged in as ${dclient.user.tag}!`);
   });
-
   dclient.on('message', msg => {
         if (msg.author.id === dclient.user.id) return;
         if (msg.channel.type === 'dm'){
